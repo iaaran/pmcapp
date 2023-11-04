f6976154dd7c76b0af8e0a84aa6755dfbad4a7b8
iaaran <aaran@murena.io>
2023-11-05 01:27:15 +0800

    Day2 coding lessons

--- app1/experiments/exercise1.py
+++ app1/experiments/exercise1.py
@@ -2,6 +2,12 @@
 # answers = ['Yes', 'No', 'Yes', 'No', 'No']
 
 # Exercise2
-my_answer = input("What is your name")
-answers = ['Yes', 'No', 'Yes', 'No', my_answer]
-print(answers)
+# my_answer = input("What is your name")
+# answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+# print(answers)
+
+#Day2 coding exercises2
+name = input("Enter your name : ")
+# Commented the while loop intentionally to avoid endless loop execution in repos
+# while True:
+print(name.capitalize())
