a775a4be06f2c81309d33187ffb6297fe3c05595
iaaran <149783791+iaaran@users.noreply.github.com>
2023-11-23 14:39:13 +0800

    Day 10

--- app1/main.py
+++ app1/main.py
@@ -1,57 +1,71 @@
 
 while True:
     # Get user input and strip space char from it
-    user_action = input("Type add or show, edit, complete or exit :")
+    user_action = input("Type add, show, edit, done or exit :")
     user_action = user_action.strip()
 
-    match user_action:
-        case "add":
-            todo = input("Enter the todo:  ") + "\n"
+    if user_action.startswith("add"):
+        todo = user_action[4:]
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            todos.append(todo)
+        todos.append(todo + '\n')
+        print(todos)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
 
-        case "show":
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+    elif user_action.startswith("show"):
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            # new_todos = [item.strip('\n) for item in todos] # This is list comprehension
-
-            for index, item in enumerate(todos):  # enumerate function is used to get index value
-                item = item.strip('\n')   # to remove the new line character
-                row = f"{index + 1}-{item}"   # f string is used for formatted display
-                print(row)
-        case "edit":
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
-
-            number = int(input("Number of the todo to edit :"))
+        for index, item in enumerate(todos):  # enumerate function is used to get index value
+            item = item.strip('\n')   # to remove the new line character
+            row = f"{index + 1}-{item}"   # f string is used for formatted display
+            print(row)
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
             number = number - 1
-            new_todo = input("Enter new todo: ") + "\n"
-            todos[number] = new_todo
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            if 0 < number < len(todos):
+                new_todo = input("Enter new todo: ") + "\n"
+                todos[number] = new_todo
+            else:
+                print("The Todo number is not in the list")
 
             with open('todos.txt', 'w') as file:
                 file.writelines(todos)
+        except ValueError:
+            print("Your command in invalid")
+            continue
 
-        case "complete":
+    elif user_action.startswith("done"):
+        try:
+            number = int(user_action[5:])
+            index = number - 1
             with open('todos.txt', 'r') as file:
                 todos = file.readlines()
-            number = int(input("Number of the todo to complete:"))
-            index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
             with open('todos.txt', 'w') as file:
                 file.writelines(todos)
 
-            print(f"Todo '{todo_to_remove}' was completed")
-
-        case "exit":
-            break
+            print(f"Todo '{todo_to_remove}' was done")
+        except IndexError:
+            print("The Todo number is not in the list")
+            continue
+        except ValueError:
+            print("Your command is not valid")
+            continue
+    elif user_action.startswith("exit"):
+        break
+    else:
+        print("Command is invalid")
 
 print("Bye!")
