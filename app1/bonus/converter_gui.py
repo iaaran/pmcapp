73f099166eae10d94794b3c3d7dadb0e0ae9855c
iaaran <149783791+iaaran@users.noreply.github.com>
2024-02-27 18:38:12 +0800

    D17 implement converter GUI app

---
+++ app1/bonus/converter_gui.py
@@ -0,0 +1,24 @@
+import PySimpleGUI as sg
+from converter14 import convert
+
+feet_label = sg.Text("Enter Feet")
+feet_input = sg.Input(key="feet")
+
+inches_label = sg.Text("Enter Inches")
+inches_input = sg.Input(key="inches")
+
+convert_button = sg.Button("Convert")
+output_label = sg.Text(" ", key="output")
+
+window = sg.Window("Convertor",
+                   layout=[[feet_label, feet_input],
+                           [inches_label, inches_input],
+                           [convert_button, output_label]])
+
+while True:
+    event, values = window.read()
+    feet = float(values['feet'])
+    inches = float(values['inches'])
+
+    result = convert(feet, inches)
+    window['output'].update(value=f"{result} m", text_color="white")
