d76ace91b8e3df47d3ec142f79e1050d2869a648
iaaran <149783791+iaaran@users.noreply.github.com>
2024-02-28 23:14:52 +0800

    D18 Update GUI app with done and exit feature

--- app1/gui.py
+++ app1/gui.py
@@ -2,6 +2,8 @@ import functions
 import PySimpleGUI as sg
 
 todos = functions.get_todos()
+
+
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip = "Enter Todo", key='todo')
 add_button = sg.Button("Add")
@@ -9,12 +11,21 @@ list_box = sg.Listbox(values=todos, key='listTodo',
                       enable_events="True", size=[45, 10])
 edit_button = sg.Button("Edit")
 done_button = sg.Button("Done")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App",
                    layout = [[label],
                              [input_box, add_button],
-                             [list_box, edit_button, done_button]],
+                             [list_box, edit_button, done_button],
+                             [exit_button]],
                    font=('Helvetica',20))
+
+
+def update_todos_window(atodos):
+    functions.set_todos(atodos)
+    window['listTodo'].update(values=atodos)
+    window['todo'].update(value='')
+
 
 while True:
     event, values = window.read()
@@ -25,26 +36,24 @@ while True:
            #  todos = functions.get_todos()
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
-            functions.set_todos(todos)
-            window['listTodo'].update(values=todos)
+            update_todos_window(todos)
+
         case 'Edit':
             todo_to_edit = values['listTodo'][0]
             new_todo = values['todo'] + '\n'
-
-            # todos = functions.get_todos()
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
-            functions.set_todos(todos)
-            window['listTodo'].update(values=todos)
+            update_todos_window(todos)
+
         case 'listTodo':
             window['todo'].update(value=values['listTodo'][0])
         case 'Done':
             todo_done = values['listTodo'][0]
+            todos.remove(todo_done)
+            update_todos_window(todos)
 
-            todos.pop(todos.index(todo_done))
-
-            functions.set_todos(todos)
-            window['listTodo'].update(values=todos)
+        case 'Exit':
+            break
         case sg.WIN_CLOSED:
             break
 
